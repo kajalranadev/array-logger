af06535014cbb8259c15fc5a9e254efe8791ef35
Kajal Rana <114815118+kajalranadev@users.noreply.github.com>
2023-11-18 05:56:43 +0000

    create a array logger

---
+++ index.js
@@ -0,0 +1,10 @@
+const myArray = [1, 2, 3, 4, 5 , "apple" , "mango" , "litchi"];
+
+function logArrayElements(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+  }
+  
+  logArrayElements(myArray);
+  
